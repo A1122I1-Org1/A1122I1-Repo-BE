8303fb9066143b10d3497cc558b4d42dbf2a115b
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2023-11-23 19:55:30 +0700

    up

--- Doanit/src/main/java/com/example/be/repository/IAccountRepository.java
+++ Doanit/src/main/java/com/example/be/repository/IAccountRepository.java
@@ -8,5 +8,8 @@ import org.springframework.stereotype.Repository;
 public interface IAccountRepository extends JpaRepository<Account,Integer> {
     Account findByUsername(String userName);
     boolean existsByUsername(String userName);
+    Account findByStudent_Id(Integer student_id);
+
+    Account findByTeacher_Id(Integer teacher_id);
 
 }
--- Doanit/src/main/java/com/example/be/service/IAccountService.java
+++ Doanit/src/main/java/com/example/be/service/IAccountService.java
@@ -1,6 +1,6 @@
 package com.example.be.service;
 
-<<<<<<< HEAD
+
 import com.example.be.entity.Account;
 
 import org.springframework.stereotype.Service;
@@ -9,13 +9,6 @@ import javax.transaction.Transactional;
 
 @Service
 @Transactional
-public interface IAccountService {
-    Account findByUserName(String userName);
-    boolean existsByUserName(String userName);
-    void changePassword(Account account);
-=======
-
-import com.example.be.entity.Account;
 
 public interface IAccountService {
 
@@ -30,5 +23,5 @@ public interface IAccountService {
     void changePassword(Account account);
 
     Account registerAccount(Account account);
->>>>>>> ad70da6e7688d82d7bb099afd5947747d9a7c490
+
 }

f600127cedc095a8e796c6befe8252986163e4b5
Nguyễn Huy <nghuy2368@gmail.com>
2023-12-08 19:00:10 +0700

    end be

--- Doanit/src/main/java/com/example/be/repository/TeacherRepository.java
+++ Doanit/src/main/java/com/example/be/repository/TeacherRepository.java
@@ -1,7 +1,10 @@
 package com.example.be.repository;
 
+import com.example.be.dto.ITeacherDto;
 import com.example.be.dto.ITeacherUpdateDTO;
 import com.example.be.entity.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -39,4 +42,19 @@ public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
             "teacher.gender as gender from teacher where teacher.phone = ?1 and teacher.delete_flag = true", nativeQuery = true)
     List<ITeacherUpdateDTO> getTeacherByPhone(String phone);
 
+    @Query(value = "SELECT  t.teacher_id As teacherId, t.name AS teacherName, t.email, t.phone, t.avatar, f.name AS facultyName \n" +
+            "FROM teacher t\n" +
+            "JOIN faculty f ON t.faculty_id = f.faculty_id\n" +
+            "where concat('MGV-',t.teacher_id,\n" +
+            "  ifnull(t.phone,''),\n" +
+            "  ifnull(t.email,''),\n" +
+            "  ifnull(t.name,''),\n" +
+            "  f.name\n) " +
+            "like concat('%', :find, '%')\n" +
+            "and t.delete_flag = 1" ,nativeQuery = true)
+    Page<ITeacherDto> getAllTeacher(String find, Pageable pageable);
+
+    @Modifying
+    @Query(value = "update teacher set teacher.delete_flag = 0 where teacher.teacher_id = ?1",nativeQuery = true)
+    void deleteTeacher(Integer id);
 }

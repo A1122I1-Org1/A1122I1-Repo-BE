cd105267df085d0d89f1a35f04019d171aec4520
TechCare <hoclephuoc1998@gmail.com>
2023-12-08 18:46:32 +0700

    hoc update2

--- Doanit/src/main/java/com/example/be/controller/GradeController.java
+++ Doanit/src/main/java/com/example/be/controller/GradeController.java
@@ -5,6 +5,7 @@ import com.example.be.service.IGradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +24,7 @@ public class GradeController {
     /**
      * Find all grade
      */
+    @PreAuthorize("hasAnyRole('TEACHER','ADMIN')" )
     @RequestMapping(value = "/get-all-grade", method = RequestMethod.GET)
     public ResponseEntity<List<Grade>> getAllGrade(){
         List<Grade> listGrade = iGradeService.getAllGrade();

6f8f0f3220db099401c63c0e33d995b2b0d95171
nghia20480 <nghia20480@gmail.com>
2023-12-10 23:15:26 +0700

    nghiaup12/10

--- Doanit/src/main/java/com/example/be/repository/GroupAccountRepository.java
+++ Doanit/src/main/java/com/example/be/repository/GroupAccountRepository.java
@@ -2,6 +2,8 @@ package com.example.be.repository;
 
 
 import com.example.be.entity.GroupAccount;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -38,4 +40,22 @@ public interface GroupAccountRepository extends JpaRepository<GroupAccount, Inte
             nativeQuery = true
     )
 void setGroupLeader(Integer studentId);
+    @Query(value = " SELECT group_account_id,date,delete_flag,status,name FROM doanit.group_account where delete_flag=0 AND name LIKE CONCAT('%', ?1, '%')" , nativeQuery = true)
+    Page<GroupAccount> findAllGroup(String find, Pageable pageable);
+    @Modifying
+    @Query(
+            value = "UPDATE `doanit`.`group_account` SET `delete_flag` = 1 WHERE (`group_account_id` = ?1);",nativeQuery = true
+    ) void deleteGroup(Integer groupId);
+    @Modifying
+    @Query(
+            value = "UPDATE `doanit`.`group_account` SET `date` = ?1 WHERE (`group_account_id` = ?2);",nativeQuery = true)
+    void updateDeadLine(String date,Integer id);
+    @Modifying
+    @Query(value = "UPDATE `doanit`.`group_account` SET `status` = 1 WHERE (`group_account_id` = ?1);",nativeQuery = true)
+    void acceptGroup(Integer groupId);
+    @Modifying
+    @Query(
+            value = "update student set student.group_account_id = null where student.student_id = ?1",
+            nativeQuery = true)
+    void deleteGroupOfStudentById(Integer id);
 }
